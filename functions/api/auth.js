768ef7451a2ead33fd7f58665ebf8a8a9a466a80
xiersun1 <atusanw3@gmail.com>
2026-07-10 03:45:38 +0900

    Limit CMS OAuth scope to public repositories

--- functions/api/auth.js
+++ functions/api/auth.js
@@ -11,7 +11,7 @@ export async function onRequest(context) {
     const redirectUrl = new URL("https://github.com/login/oauth/authorize");
     redirectUrl.searchParams.set("client_id", clientId);
     redirectUrl.searchParams.set("redirect_uri", `${url.origin}/api/callback`);
-    redirectUrl.searchParams.set("scope", "repo user");
+    redirectUrl.searchParams.set("scope", "public_repo");
     redirectUrl.searchParams.set(
       "state",
       crypto.getRandomValues(new Uint8Array(12)).join("")
